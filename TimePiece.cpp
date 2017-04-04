3ab5d5861131d0207544e730119a8db034369226
Jakson Kallio <jakson@protonmail.com>
2017-04-04 13:30:15 -0500

    Return requested timeframe

--- TimePiece.cpp
+++ TimePiece.cpp
@@ -1,5 +1,25 @@
+#include <ctime>
 #include "TimePiece.h"
 
-time_t TimePiece::getTime() const{
-	return time(0);
+unsigned int TimePiece::getTime(timeframe_t timeframe) const{
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+
+	if(timeframe == SEC){
+		return ltm->tm_sec;
+	}else if(timeframe == MIN){
+		return ltm->tm_min;
+	}else if(timeframe == HOUR){
+		int hour = ltm->tm_hour;
+
+		if(!clock24h && hour > 12){
+			hour -= 12;
+		}
+
+		return hour;
+	}else{
+		throw 100;
+	}
+
+	return 0;
 }
