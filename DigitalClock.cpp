fcf2103814b86b4622b341d86c7e711882ab855c
Jakson Kallio <jakson@protonmail.com>
2017-04-05 12:52:02 -0500

    Digit storage issues fixed

--- DigitalClock.cpp
+++ DigitalClock.cpp
@@ -2,31 +2,91 @@
 #include <tuple>
 #include "DigitalClock.h"
 
-
+bool const DigitalClock::digits[10][5][3] =
+{{{1, 1, 1},
+	{1, 0, 1},
+	{1, 0, 1},
+  {1, 0, 1},
+	{1, 1, 1}},
+ {{0, 1, 0},
+	{0, 1, 0},
+	{0, 1, 0},
+  {0, 1, 0},
+	{0, 1, 0}},
+ {{1, 1, 1},
+	{0, 0, 1},
+	{1, 1, 1},
+  {1, 0, 0},
+	{1, 1, 1}},
+ {{1, 1, 1},
+	{0, 0, 1},
+	{0, 1, 1},
+  {0, 0, 1},
+	{1, 1, 1}},
+ {{1, 0, 1},
+	{1, 0, 1},
+	{1, 1, 1},
+  {0, 0, 1},
+	{0, 0, 1}},
+ {{1, 1, 1},
+	{1, 0, 0},
+	{1, 1, 1},
+  {0, 0, 1},
+	{1, 1, 1}},
+ {{1, 1, 1},
+	{1, 0, 0},
+	{1, 1, 1},
+  {1, 0, 1},
+	{1, 1, 1}},
+ {{1, 1, 1},
+	{0, 0, 1},
+	{0, 0, 1},
+  {0, 0, 1},
+	{0, 0, 1}},
+ {{1, 1, 1},
+	{1, 0, 1},
+	{1, 1, 1},
+  {1, 0, 1},
+	{1, 1, 1}},
+ {{1, 1, 1},
+	{1, 0, 1},
+	{1, 1, 1},
+  {0, 0, 1},
+	{0, 0, 1}}};
 
 void DigitalClock::refreshDisplay(){
 	int sec = getTime(SEC);
 	int min = getTime(MIN);
 	int hour = getTime(HOUR);
-	int term_width;	
-	int term_height;	
+	int term_width;
+	int term_height;
 
 	std::tie(term_width, term_height) = gatherTerminalStats();
-	
-	std::cout << term_width << "\n";
-	std::cout << term_height << "\n";
+
+	for(unsigned int i = 1; i < 5; ++i){
+		if(hour >= 10){
+			//std::cout << getDisplayDigitRow(, i);
+		}
+
+	}
 }
 
 std::string DigitalClock::getDisplayDigitRow(unsigned int number, unsigned int row) const{
 	std::string rowstr = "";
 
-	for(unsigned int i = 0; i <= 3; ++i){
+	if(!(row >= 1 && row <= 5 && number >= 0 && number <= 9)){
+		throw 101;
+	}
+
+	row -= 1;
+
+	for(unsigned int i = 0; i < 3; ++i){
 		if(digits[number][row][i]){
 			rowstr += draw_character;
 		}else{
-			rowstr += ' ';		
+			rowstr += ' ';
 		}
 	}
-	
+
 	return rowstr;
 }
